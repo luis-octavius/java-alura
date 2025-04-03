8c73a04cad59a0272c25e3238d48eb6ea8e18e43
Big-Plato <luisoctavius.sc@gmail.com>
2025-04-03 20:33:58 -0300

    Novo arquivo para calcular media

---
+++ src/desafio-1/Media.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Media {
+    public static void main(String[] args) {
+        Scanner nota1 = new Scanner(System.in);
+        System.out.println("Digite a primeira nota: \n");
+        Scanner nota2 = new Scanner(System.in);
+        System.out.println("Digite a segunda nota: \n");
+
+        double nota = Double.parseDouble(nota1.nextLine());
+        double notaDois = Double.parseDouble(nota2.nextLine());
+
+        double media = (nota + notaDois) / 2;
+
+        System.out.println("A media é %.2f".formatted(media));
+
+        char passed = 'A';
+        char notPassed = 'F';
+        String passou = "Conseguiste, Parabéns";
+        String nãoPassou = "Infelizmente você foi eliminado.";
+
+        if (media >= 7) {
+            System.out.println(passed + ": " + passou);
+        } else {
+            System.out.println(notPassed + ": " + nãoPassou);
+        }
+
+    }
+}
