18fcc3f0ad3e7c59618014e6b6385c0f95f2fa10
Big-Plato <luisoctavius.sc@gmail.com>
2025-04-03 22:08:48 -0300

    Novo arquivo para converter dolares em reais

---
+++ src/desafio-1/ConversaoDolaresReais.java
@@ -0,0 +1,11 @@
+public class ConversaoDolaresReais {
+    public static void main(String[] args) {
+        double precoProduto = 10.99;
+        int quantidade = 5;
+
+        double valorEmDolares = 5.99;
+        double taxaDeConversão = 4.94;
+        double conversion = valorEmDolares * taxaDeConversão;
+        System.out.println(conversion);
+    }
+}
