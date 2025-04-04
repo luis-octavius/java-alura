662aca5083ee6de3e4f6548de1f425486a859c2c
Big-Plato <luisoctavius.sc@gmail.com>
2025-04-03 22:08:38 -0300

    Novo arquivo para calcular desconto

---
+++ src/desafio-1/CalculoDesconto.java
@@ -0,0 +1,12 @@
+public class CalculoDesconto {
+    public static void main(String[] args) {
+        double precoOriginal = 45.0;
+        double percentualDesconto = 10.0;
+
+        double valorDesconto = (percentualDesconto / 100.0) * precoOriginal;
+        double precoNovo = precoOriginal - valorDesconto;
+        System.out.println("Preço original: R$" + precoOriginal);
+        System.out.println("Desconto: R$" + valorDesconto);
+        System.out.println("Preço com desconto: R$" + precoNovo);
+    }
+}
