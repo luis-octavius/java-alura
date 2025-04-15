31a7f790424e0d8f9136789ec1266a54d3b499b8
Big-Plato <luisoctavius.sc@gmail.com>
2025-04-15 18:40:57 -0300

    novos arquivos

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
 import java.util.Scanner;
+import com.Student;
+import com.Livro;
 
-class Perfil {
+public class Main {
     public static void main(String[] args) {
-        Scanner name = new Scanner(System.in);
-        System.out.println("Digite o seu nome: \n");
+//        Student student = new Student();
+//        student.pushNote(10);
+//        student.pushNote(9);
+//        student.pushNote(8);
+//        student.pushNote(7);
+//
+//        System.out.println("Media: " + student.calculateMedia());
 
-        String userName = name.nextLine();
-        System.out.println("Olá, " + userName);
-
-        System.out.println("Tudo bem?");
+        Livro livro = new Livro();
+        livro.setTitle("Thus Spoke Zarathustra");
+        livro.setAuthor("Nietzsche");
+        System.out.println(livro.getDetails());
     }
 }
--- src/com/Loop.java
+++ src/com/Loop.java
@@ -1,3 +1,6 @@
+package com;
+import java.util.Scanner;
+
 public class Loop {
   public static void main(String[] args) {
     Scanner leitura = new Scanner(System.in);
@@ -5,11 +8,10 @@ public class Loop {
     double avaliacao = 0;
 
     for (int i = 0; i < 3; i++) {
-      System.out.println("Diga sua avaliação para o filme ");
-      nota = leitura.nextDouble();
+      System.out.println("Diga sua avaliacao para o filme ");
+      double nota = leitura.nextDouble();
       mediaAvaliacao += nota;
     }
-    
-    System.out.println("Média de avaliações: ", + mediaAvaliacao);
+    System.out.println("Média de avaliações: " + mediaAvaliacao);
   }
 }
